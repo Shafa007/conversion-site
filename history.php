da9ae0bf46a1cdeddc3040e3c570dbbfa5fef55d
Shafa007 <80678467+Shafa007@users.noreply.github.com>
2021-06-22 11:14:09 +0600

    history commit

---
+++ history.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>History</title>
+</head>
+<body>
+	<h2>Page 3 [History]</h2>
+	<h3>Conversion Site</h3>
+	
+    <a href="http://localhost/home.php" target="_blank">1.Home </a>
+    <a href="http://localhost/conversionrate.php" target="_blank">2.Conversion Rate </a>
+    <a href="http://localhost/history.php" target="_blank">3.History </a>
+    <br><br>
+    <label for="converter">Conversion History: </label>
+    <br><br>
+		<textarea id="history" name="history"></textarea>
+
+	</body>
+</html>
