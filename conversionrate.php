81bb9bdc81b208d94f3b78b5ad77ddd319691afc
Shafa007 <80678467+Shafa007@users.noreply.github.com>
2021-06-22 11:13:27 +0600

    conversionrate commit

---
+++ conversionrate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Conversion Rate</title>
+</head>
+<body>
+	<h2>Page 2 [Conversion Rate]</h2>
+	<h3>Conversion Site</h3>
+	
+    <a href="http://localhost/home.php" target="_blank">1.Home </a>
+    <a href="http://localhost/conversionrate.php" target="_blank">2.Conversion Rate </a>
+    <a href="http://localhost/history.php" target="_blank">3.History </a>
+    <br><br>
+    <label for="converter">Conversion Rate: </label>
+    <br><br>
+		<select id="dropdown" name="dropdown"  value="dropdown">
+		    <option value="feettoinch">feet to inch</option> 
+		</select>
+	
+		<input type="text" id="value" name="value" value="1">
+		<input type="text" id="result" name="result" value="12">
+
+	</body>
+</html>
